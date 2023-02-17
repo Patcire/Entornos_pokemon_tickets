5cf68ced5a1eb356da03310fb4ce5e0fb1228947
Patricio <pcifrey501@g.educaand.es>
2023-02-17 18:57:05 +0100

    resolución del ticket AA0001, creación de clase tipo que reciba dato tipo String

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,6 @@
 fun main(args: Array<String>) {
-    var prueba2=Pokemon("electrico")
-    println(prueba2.tipo_pk)
-    var prueba=Pokemon("caramelo")
+    //var prueba2=Pokemon("electrico")
+    //println(prueba2.tipo_pk)
+    //var prueba=Pokemon("caramelo")
 
 }
---
+++ src/main/kotlin/Tipo.kt
@@ -0,0 +1,9 @@
+class Tipo(tipo:String) {
+
+    var tipo_pk="defecto"
+
+    init {
+        tipo_pk=tipo
+    }
+
+}
