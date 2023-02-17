75259d7d387cfdf775629000c6776914f5abbf21
Patricio <pcifrey501@g.educaand.es>
2023-02-17 20:17:42 +0100

    añadido init en clase pokemon

---
+++ src/main/kotlin/Pokemon.kt
@@ -0,0 +1,13 @@
+class Pokemon(nombre:String,vida:Int,tipo:String) {
+
+        var nombre="defecto"
+        var vida=100
+        var tipo="defecto"
+
+        init {
+            this.nombre=nombre
+            this.vida=vida
+            this.tipo=tipo
+        }
+
+}
