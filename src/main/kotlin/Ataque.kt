483b638356d8809d705d2e599cdc3cdb8cef918a
Patricio <pcifrey501@g.educaand.es>
2023-02-17 20:19:42 +0100

    creación de clase ataque

--- src/main/kotlin/Ataque.kt
+++ src/main/kotlin/Ataque.kt
@@ -1,2 +1,10 @@
-class Ataque(dano:Int,) {
+class Ataque(danio:Int, tipo: String) {
+
+    var danio=20
+    var tipo="defecto"
+
+    init {
+        this.danio=danio
+        this.tipo=tipo
+    }
 }
