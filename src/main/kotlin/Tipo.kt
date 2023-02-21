0326cd9e1726a815dd6dae21c1ac7bf26fbc0d9c
Patricio <pcifrey501@g.educaand.es>
2023-02-21 14:17:53 +0100

    quitado las listas tipos a b c d

--- src/main/kotlin/Tipo.kt
+++ src/main/kotlin/Tipo.kt
@@ -4,14 +4,6 @@ class Tipo(tipo:String) {
         "fantasma", "fuego", "hada", "hielo", "lucha", "normal", "planta", "psiquico", "roca",
         "siniestro", "tierra", "veneno", "volador")
 
-    var tipos_a= mutableListOf<String>("acero", "agua", "bicho", "dragon", "electrico",
-        "fantasma", "fuego")
-
-    var tipos_b= mutableListOf<String>("hada", "hielo", "lucha", "normal", "planta")
-    var tipos_c= mutableListOf<String>("psiquico", "roca", "siniestro")
-    var tipos_d= mutableListOf<String>("tierra", "veneno", "volador")
-
-
 
     var tipo_pk="defecto"
         get() {
